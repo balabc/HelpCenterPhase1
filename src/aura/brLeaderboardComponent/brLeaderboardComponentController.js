3588b1348ef3aa42c3adee7370d9f06be3f15e52
Victor Senko <victor.s@websolo.ca>
2017-11-10 10:06:54 -0500

    BIG-5 10/11/2017 Swype Users in progress

--- src/aura/brLeaderboardComponent/brLeaderboardComponentController.js
+++ src/aura/brLeaderboardComponent/brLeaderboardComponentController.js
@@ -2,4 +2,13 @@
 	doInit : function(component, event, helper) {
         helper.getData(component, 'v.userLeaderboard', 'c.getUserLeaderboard', false);
 	},
+    jsLoaded : function(component, event, helper) {
+        window.mySwiper = new Swiper ('.swiper-container', {
+            slidesPerView: 'auto'
+        });
+        window.addEventListener('resize', function(){
+            window.mySwiper.update();
+        } , false );
+        
+    }
 })
