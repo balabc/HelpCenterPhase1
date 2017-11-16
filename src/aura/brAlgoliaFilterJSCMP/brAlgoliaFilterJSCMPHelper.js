1bd63f22e93597abcd34520b95c80a44c007daf3
Victor Senko <victor.s@websolo.ca>
2017-11-16 15:38:50 +0300

    BIG-13 17/11/2017 init

---
+++ src/aura/brAlgoliaFilterJSCMP/brAlgoliaFilterJSCMPHelper.js
@@ -0,0 +1,16 @@
+({
+	toChangeFilter : function(component) {
+		var changeFilter = component.getEvent("eventFromBrAlgoliaFilter"),
+            currentFilter = component.get("v.currentFilter"),
+            filter = component.get("v.brFilter");
+        console.log(filter);
+        
+        changeFilter.setParams({
+            "brFilter" : {
+                type: currentFilter,
+                values: filter[currentFilter]
+            }
+        });
+        changeFilter.fire();
+	}
+})
