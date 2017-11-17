f864003793dfdcaa0ef56a8c7c279d4da6ed2f61
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-11-17 06:40:29 +0200

    BIG-9 fix ready state

--- src/aura/brHeaderContact/brHeaderContactHelper.js
+++ src/aura/brHeaderContact/brHeaderContactHelper.js
@@ -5,9 +5,10 @@
                 var messageObject = JSON.parse(e.data);
                 console.log('###messageObject:', messageObject);
 
-                //if(messageObject['action'] == 'page-status-ready'){
-                if(messageObject['action'] == 'page-status'){
+                if(messageObject['action'] == 'page-status-ready'){
                     component.set('v.frameStatusReady', true);
+                }
+                if(messageObject['action'] == 'page-status'){
                     //messageObject['description']
                     //messageObject['indicator']
                 }
@@ -21,7 +22,8 @@
         var ready = component.get('v.frameStatusReady');
         var frameID = component.get('v.frameID');
         var frameURL = component.get('v.frameURL');
-        if(!ready){
+        //console.log('ready:', ready);
+        if(ready){
             try{
                 var frameWindow = document.getElementById(frameID);
                 if(frameWindow != null){
