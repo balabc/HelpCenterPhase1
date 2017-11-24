dd3e198fbc50bcf72dfab6dcb560723c56d0079d
Victor Senko <victor.s@websolo.ca>
2017-11-24 11:00:16 +0300

    BIG-12 Add user menu, add test class

--- src/aura/brMobileNavMenu/brMobileNavMenuController.js
+++ src/aura/brMobileNavMenu/brMobileNavMenuController.js
@@ -28,11 +28,38 @@
             document.getElementsByClassName('header-mobile__menu-icon')[0].addEventListener('click', funcMenu);
             
         });
-        helper.fillPhoneList(component);        
+        helper.fillPhoneList(component);    
+        helper.getUserInfo(component);    
+	},
+	changeUser : function(component, event, helper) {
+		var items = component.get('v.menuList'),
+			us = component.get('v.user');
+		if (us) {
+			items.pop();
+			items.push({
+	            id: 'user', 
+	            label: us.name, 
+	            picture: '<span class="header-mobile__menu-shape"><img src="'+us.photoUrl+'" class="header-mobile__menu-userpic" alt=""></span>',
+	            subMenu: [                       
+	                {id: 'profile', label: 'Profile', url: '/support/s/profile/' + us.id},
+	                {id: 'my_messages', label: 'My Messages', url: '/support/s/messages/Home'},
+	                {id: 'my_feed', label: 'My Feed', url: '/support/s/feed'},
+	                {id: 'my_cases', label: 'My Cases', url: '/support/s/case'},
+	                {id: 'my_files', label: 'My Files', url: '/support/s/MyFeed'},
+	                {id: 'log_out', label: 'Log Out', url: '/support/secur/logout.jsp'}
+	            ]
+	        });		
+		}
+        component.set('v.menuList', items);
+        component.set('v.currentMenu', {
+            lvl: 1, 
+            title: '',
+            titles: [],
+            items: [0, items]
+        });
 	},
     changeMenu : function(component, event, helper) {
         var items = component.get('v.menuItems');
-        
         items.push({id: 'contact', label: 'Contact', subMenu: [
             {id: 'articles', label: 'Articles'},
             {id: 'learning_guides', label: 'Learning Guides'},
@@ -42,6 +69,12 @@
             {id: 'phone_support', label: 'Phone Support', subMenu: []}
         ]});
         items.push({id: 'notifications', label: 'Notifications'});
+        items.push({
+            id: 'login', 
+            label: 'Login',
+            url: '/support/s/login/'  
+        });
+        
         
         component.set('v.menuList', items);
         component.set('v.currentMenu', {
--- src/aura/brMobileNavMenu/brMobileNavMenuHelper.js
+++ src/aura/brMobileNavMenu/brMobileNavMenuHelper.js
@@ -25,5 +25,29 @@
         }catch(e){
             console.log('tryE:', e);
         }
-    }
+    },
+    getUserInfo : function(component){
+        try {
+            var action = component.get("c.getUserInfo");
+            action.setCallback(this, function(response){
+                var state = response.getState();
+                if (state === "SUCCESS") {      
+                    component.set("v.user", response.getReturnValue());
+                }else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (errors) {
+                        if (errors[0] && errors[0].message) {
+                            console.log("Error message: " + errors[0].message);
+                        }
+                    } else {
+                        console.log("Unknown error");
+                    }
+                }
+            });
+            $A.enqueueAction(action);
+
+        }catch(e){
+            console.log('tryE:', e);
+        }
+    }    
 })
