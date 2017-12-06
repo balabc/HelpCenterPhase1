c124df703c239dc50e79c7388e77075fed3f74a8
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-12-06 19:09:03 +0200

    BIG-22 on main menu item category click it opens corresponding url

--- src/aura/brMainNavMenu/brMainNavMenuController.js
+++ src/aura/brMainNavMenu/brMainNavMenuController.js
@@ -17,6 +17,56 @@
             }
         };
     },
+    onCategoryClick : function(component, event) {
+        try {
+            //var sss = component.get('v.menuItems');
+            //console.log('menuItems',sss);
+            //var exitingURL = event.target.dataset.url;
+            //if(exitingURL){
+            //    console.log('Menu Category - using exiting URL' + exitingURL);
+            //    var urlEvent = $A.get("e.force:navigateToURL");
+            //    urlEvent.setParams({
+            //      "url": exitingURL
+            //    });
+            //    urlEvent.fire();
+            //}else{
+                var action = component.get("c.getURLByMenuLabel");
+                var itemLabel = event.target.dataset.label;
+                if (itemLabel) {
+                    action.setParams({
+                        fullurl : window.location.href,
+                        label : itemLabel
+                    });
+                    action.setCallback(this, function(response){
+                        var state = response.getState();
+                        if (state === "SUCCESS") {
+                            var target = response.getReturnValue();
+                            if(target){
+                                var urlEvent = $A.get("e.force:navigateToURL");
+                                urlEvent.setParams({
+                                  "url": target
+                                });
+                                urlEvent.fire();
+                            }
+                        }else if (state === "ERROR") {
+                            var errors = response.getError();
+                            console.log("Errors: ", errors);
+                            if (errors) {
+                                if (errors[0] && errors[0].message) {
+                                    console.log("Error message: " + errors[0].message);
+                                }
+                            } else {
+                                console.log("Unknown error");
+                            }
+                        }
+                    });
+                    $A.enqueueAction(action);
+                }
+            //}
+        }catch(e){
+            console.log('tryE:', e);
+        }
+    },
     onClick : function(cmp, event) {
         var id = event.target.dataset.menuItemId;
         if (id) {
