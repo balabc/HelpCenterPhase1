0f52f9415a70725c000fe67604c30038b867cdc3
Victor Senko <victor@webcity.by>
2017-12-10 13:37:49 +0300

    BIG-24 brArticleContent

---
+++ src/aura/brArticleContent/brArticleContentHelper.js
@@ -0,0 +1,29 @@
+({
+	getDataForArticle: function(component) {
+		var action = component.get("c.getDataForArticle"),
+            articleId = component.get('v.recordId');
+        
+        action.setParams({
+            'articleId': articleId
+        });
+        
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var data = response.getReturnValue();
+                console.log(data);
+                component.set("v.articleType", data.articleType);
+                component.set("v.dataCategory", data.dataCategory);
+                
+                var brCategoriesCMP = component.find('brCategoriesCMP')
+                brCategoriesCMP.set('v.data', {
+                    id: articleId,
+                    objectName: data.articleType,
+                    dataCategory: data.dataCategory
+                });
+                brCategoriesCMP.changeData();
+            }
+        });
+        $A.enqueueAction(action);
+	}
+})
