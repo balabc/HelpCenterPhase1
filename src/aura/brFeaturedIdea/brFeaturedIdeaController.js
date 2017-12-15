0e98e3d924ad9076b0d355cf1d589c782bd8ee84
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-12-15 13:33:09 +0200

    BIG-32 #resolve #comment fix login click and finished test

--- src/aura/brFeaturedIdea/brFeaturedIdeaController.js
+++ src/aura/brFeaturedIdea/brFeaturedIdeaController.js
@@ -13,24 +13,17 @@
             urlEvent.fire();
         }
     },
-    logInClick : function(component, event, helper) {
-        //console.log('logInClick');
-        if(typeof $A.get("$SfdcSite") !== 'undefined'){
-            var host = window.location.host;
-            var communityPrefix = $A.get("$SfdcSite").pathPrefix;
-            var urlEvent = $A.get("e.force:navigateToURL");
-            urlEvent.setParams({
-              "url": 'https://' + host + communityPrefix + '/s/login/'//TODO: clarify right way for this
-            });
-            urlEvent.fire();
-        }
-    },
     doInit : function(component, event, helper) {
         //console.log('Featured Idea');
         if(typeof $A.get("$SfdcSite") !== 'undefined'){
-	        var communityPrefix = $A.get("$SfdcSite").pathPrefix;
-
-	        console.log('communityPrefix:'+communityPrefix);
+            try{
+	            var communityPrefix = $A.get("$SfdcSite").pathPrefix;
+                var host = window.location.host;
+                component.set('v.loginURL', 'https://' + host + communityPrefix + '/s/login/');
+            }catch(ee){
+                console.log('tryE:', ee);
+            }
+	        //console.log('communityPrefix:'+communityPrefix);
 	    }
         helper.getComponentData(component);
     }
