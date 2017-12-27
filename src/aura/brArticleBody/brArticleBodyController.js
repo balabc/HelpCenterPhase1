d973c885e12aec1d600e081291d99f31a8af6aa7
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-12-27 16:08:23 +0200

    BIG-52  upd: brArticleBodyController.js

--- src/aura/brArticleBody/brArticleBodyController.js
+++ src/aura/brArticleBody/brArticleBodyController.js
@@ -12,6 +12,27 @@
 
             if (state === "SUCCESS") {
 
+            }else if (state === "ERROR") {
+                var errors = response.getError();
+                var error_msg = '';
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        if(errors[0].message == 'access_error'){
+                            error_msg = $A.get("$Label.c.hCommunityFLSAccess");
+                        }else{
+                            error_msg = errors[0].message;
+                        }
+                    }
+                }
+                if(error_msg.length === 0){
+                    error_msg = $A.get("$Label.c.hUnknownError");
+                }
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    mode: "sticky",
+                    message: error_msg
+                });
+                toastEvent.fire();
             }
         });
 
