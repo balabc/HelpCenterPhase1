425750b3adb52ee40170a595df0b616d0eda8c20
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-12-27 16:42:29 +0200

    BIG-52 responce error handle

--- src/aura/brArticleContent/brArticleContentHelper.js
+++ src/aura/brArticleContent/brArticleContentHelper.js
@@ -24,6 +24,27 @@
                     });
                     brCategoriesCMP.changeData();
                 }
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                var error_msg = '';
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        if (errors[0].message == 'access_error') {
+                            error_msg = $A.get("$Label.c.hCommunityFLSAccess");
+                        } else {
+                            error_msg = errors[0].message;
+                        }
+                    }
+                }
+                if (error_msg.length === 0) {
+                    error_msg = $A.get("$Label.c.hUnknownError");
+                }
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    mode: "sticky",
+                    message: error_msg
+                });
+                toastEvent.fire();
             }
         });
         $A.enqueueAction(action);
@@ -42,6 +63,27 @@
             if (state === "SUCCESS") {
                 var data = response.getReturnValue();
                 component.set("v.article", data);
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                var error_msg = '';
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        if (errors[0].message == 'access_error') {
+                            error_msg = $A.get("$Label.c.hCommunityFLSAccess");
+                        } else {
+                            error_msg = errors[0].message;
+                        }
+                    }
+                }
+                if (error_msg.length === 0) {
+                    error_msg = $A.get("$Label.c.hUnknownError");
+                }
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    mode: "sticky",
+                    message: error_msg
+                });
+                toastEvent.fire();
             }
         });
         $A.enqueueAction(action);
