d7c3d16ff44a22279bd9a7dbe99e6c283287804f
Dmitry Dubnovitsky <dmitry.dubnovitsky@gmail.com>
2017-12-28 16:25:15 +0300

    fix open new window on user menu

--- src/aura/brUserMenuItem/brUserMenuItemController.js
+++ src/aura/brUserMenuItem/brUserMenuItemController.js
@@ -1,21 +1,47 @@
 ({
     openNewWindow: function (cmp, event, helper) {
-        var urlEvent = $A.get("e.force:navigateToURL"),
-            menuItem = cmp.get('v.menuItem'),
-            url = menuItem.url;
+        var menuItem = cmp.get('v.menuItem'),
+            urlEvent = $A.get("e.force:navigateToURL"),
+            location = window.location.href,
+            url =  menuItem.url,
+            existOldUrlWithProfileOrSettings = false;
+
+        if ((location.includes('profile') || location.includes('settings')) && cmp.get('v.menuItem').new_window) {
+            existOldUrlWithProfileOrSettings = true;
+            var menuItemFind;
+
+            if (location.includes('profile')) {
+                menuItemFind = 'profile';
+            }
+            if (location.includes('settings')) {
+                menuItemFind = 'settings';
+            }
+            var n = location.indexOf(menuItemFind);
+            location = location.substring(0, n != -1 ? n : location.length);
+            url = location + url;
+        }
 
         if (menuItem.url == 'profile' || menuItem.url == 'settings') {
-            url += '/'+cmp.get('v.userId');
+            url += '/' + cmp.get('v.userId');
+        } else {
+            url = menuItem.url;
         }
 
-        if (!cmp.get('v.menuItem').new_window) {
-            url = '/' + url;
+        if (cmp.get('v.menuItem').new_window) {
+            if (existOldUrlWithProfileOrSettings) {
+                window.open(url);
+            } else {
+                urlEvent.setParams({
+                    "url": url
+                });
+                urlEvent.fire();
+            }
+        } else {
+            urlEvent.setParams({
+                "url": '/' + url
+            });
+
+            urlEvent.fire();
         }
-
-        urlEvent.setParams({
-            "url": url
-        });
-
-        urlEvent.fire();
     }
 })
