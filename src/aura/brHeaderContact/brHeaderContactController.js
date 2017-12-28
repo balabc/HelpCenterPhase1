682d38d03ec1b8dc42b67dbd3335b4109037f5ee
Rostyslav Volonchuk <mr.rostys@gmail.com>
2017-12-28 17:36:02 +0200

    BIG-60 #resolve

--- src/aura/brHeaderContact/brHeaderContactController.js
+++ src/aura/brHeaderContact/brHeaderContactController.js
@@ -5,16 +5,22 @@
         document.getElementById("contact-lvl-2").classList.remove('contact-lvl-2--active');
     },
     showMe : function(component, event, helper) {
-        helper.askPageStatus(component);
         try{
+            var openCounter = component.get('v.openCounter');
+            component.set('v.openCounter', ++openCounter);
+            helper.askPageStatus(component);
+            if (openCounter === 1) {//first time open
+                var cmpCQ = component.find('commonQuestions');
+                cmpCQ.reInit(component, event, helper);
+            }
             var contactPanel = document.getElementsByClassName( 'header__contact' )[0];//TODO: possible bug if structure will change
             contactPanel.classList.add( 'header__contact--active' );
         }catch(ee){
-            console.log(ee.message);
+            console.error(ee.message);
         }
     },
     doInit : function(component, event, helper) {
-        helper.fillPhoneList(component);
+        //helper.fillPhoneList(component);
     },
     openEmailSupport : function(component, helper){
         var urlEvent = $A.get("e.force:navigateToURL");
@@ -23,7 +29,11 @@
         });
         urlEvent.fire();
     },
-    openPhoneSupport : function(component){
+    openPhoneSupport : function(component, event, helper){
+        var phoneCount = component.get('v.phoneCount');
+        if ( phoneCount === 0 ) {
+            helper.fillPhoneList(component);
+        }
         document.getElementById("contact-lvl-2").classList.add('contact-lvl-2--active');
     },
     closePhoneSupport : function(component){
