062d943629e11a62116158c673770297794cec99
Victor Senko <victor@webcity.by>
2018-01-08 09:55:10 +0300

    BIG-70 #resolve #comment done task

--- src/aura/brCategoriesCMP/brCategoriesCMPHelper.js
+++ src/aura/brCategoriesCMP/brCategoriesCMPHelper.js
@@ -31,24 +31,86 @@
         if (!!url) {
             url = url.split('/');
             url = url[url.length - 1];
-           //console.log(url);
+            //console.log(url);
         }
 
-        if (!!selectId) { 
-           //console.log(selectId, articleType, dataCategoryName);
+        action.setParams({
+            articleType: articleType,
+            dataCategoryName: dataCategoryName
+        });
+
+        var fSelectCategory = function(url, id, data) {
+            var flag_selected = false,
+                c1, c2, c3;
+            console.log(url, id);
+            c1 = data;
+            for (var ck1 in c1) {
+                if (c1[ck1].articles.length > 0) {
+                    for (var i1 in c1[ck1].articles) {
+                        console.log(c1[ck1].articles[i1]);
+                        if ((c1[ck1].articles[i1].article_id === id) || (c1[ck1].articles[i1].UrlName === url)) {
+                            flag_selected = true;
+                            c1[ck1].articles[i1].active = true;
+                            break;
+                        }
+                    }
+                }
+                c2 = c1[ck1].categories;
+                for (var ck2 in c2) {
+                    if (c2[ck2].articles.length > 0) {
+                        for (var i2 in c2[ck2].articles) {
+                            console.log(c2[ck2].articles[i2]);
+                            if ((c2[ck2].articles[i2].article_id === id) || (c2[ck2].articles[i2].UrlName === url)) {
+                                flag_selected = true;
+                                c2[ck2].articles[i2].active = true;
+                                break;
+                            }
+                        }
+                    }
+                    c3 = c2[ck2].categories;
+                    for (var ck3 in c3) {
+                        if (c3[ck3].articles.length > 0) {
+                            for (var i3 in c3[ck3].articles) {
+                                console.log(c3[ck3].articles[i3]);
+                                if ((c3[ck3].articles[i3].article_id === id) || (c3[ck3].articles[i3].UrlName === url)) {
+                                    flag_selected = true;
+                                    c3[ck3].articles[i3].active = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (flag_selected) {
+                            c3[ck3].active = true;
+                            break;
+                        }
+                    }
+                    if (flag_selected) {
+                        c2[ck2].active = true;
+                        break;
+                    }
+                }
+                if (flag_selected) {
+                    c1[ck1].active = true;
+                    break;
+                }
+            }
+        };
+
+        /*if (!!selectId) {
+            //console.log(selectId, articleType, dataCategoryName);
             action.setParams({
-                url: url,
-                selectId : selectId,
+                //url: url,
+                //selectId : selectId,
                 articleType: articleType,
                 dataCategoryName: dataCategoryName
             });
         } else {
             action.setParams({
-                url: url,
+                //url: url,
                 articleType: articleType,
                 dataCategoryName: dataCategoryName
             });
-        }
+        }*/
 
         action.setStorable();
                
@@ -56,8 +118,15 @@
             var state = response.getState();
             if (component.isValid() && state === "SUCCESS") {
                 var data = response.getReturnValue();
-               console.log(data);
+                console.log(data);
+                if (!!selectId) {
+                    fSelectCategory(url, selectId, data);
+                } else {
+                    fSelectCategory(url, selectId, data);
+                }
+                console.log(data);
                 component.set("v.items", data);
+
             } else {
                //console.log(response);
             }
