661f63282617aceaa37a1cfc5416aa7f90ddf72c
Dmitry Dubnovitsky <dmitry.dubnovitsky@gmail.com>
2018-01-24 17:49:29 +0300

    BIG-96 fix prefooter on Profile page

--- src/aura/brPreFooter/brPreFooterController.js
+++ src/aura/brPreFooter/brPreFooterController.js
@@ -1,18 +1,29 @@
 ({
     doneRendering: function(component, event, helper) {
-        window.setTimeout(
-            $A.getCallback(function() {
-                var documentHeight = document.body.scrollHeight,
-                    windowHeight = window.innerHeight,
-                    target = component.find('prefooter');
-
-                if (documentHeight === windowHeight || documentHeight === 0) {
+        var documentHeight = document.body.scrollHeight,
+            windowHeight = window.innerHeight,
+            target = component.find('prefooter');
+        if ((documentHeight > windowHeight) && component.get('v.defineLargeContentSize')) {
+            return;
+        }
+        if (window.location.href.includes('profile')) {
+            if (documentHeight > windowHeight) {
+                target.getElement().style.display = 'block';
+            } else {
+                target.getElement().style.display = 'none';
+            }
+        } else {
+            if (documentHeight === windowHeight || documentHeight === 0) {
+                if (!component.get('v.defineSmallContentSize')) {
                     $A.util.addClass(target, 'smallContent');
-                } else {
-                    $A.util.removeClass(target, 'smallContent');
+                    component.set('v.defineSmallContentSize', true);
                 }
-            }), 500
-        );
+            } else {
+                $A.util.removeClass(target, 'smallContent');
+                component.set('v.defineLargeContentSize', true);
+            }
+        }
+
     },
     showNewPage: function (cmp, event, helper) {
         var urlEvent = $A.get("e.force:navigateToURL");
