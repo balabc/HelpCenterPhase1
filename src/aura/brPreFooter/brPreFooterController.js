8dc0bb9ff4c6f65728909e5a6923d71af10fb840
Dmitry Dubnovitsky <dmitry.dubnovitsky@gmail.com>
2018-02-05 15:18:32 +0300

    BIG-102 fix prefooter for mobile

--- src/aura/brPreFooter/brPreFooterController.js
+++ src/aura/brPreFooter/brPreFooterController.js
@@ -1,26 +1,28 @@
 ({
     doneRendering: function(component, event, helper) {
-        var documentHeight = document.body.scrollHeight,
-            windowHeight = window.innerHeight,
-            target = component.find('prefooter');
-        if ((documentHeight > windowHeight) && component.get('v.defineLargeContentSize')) {
-            return;
-        }
-        if (window.location.href.includes('profile')) {
-            if (documentHeight > windowHeight) {
-                target.getElement().style.display = 'block';
-            } else {
-                target.getElement().style.display = 'none';
+        if ($A.get("$Browser.formFactor") !== 'PHONE') {
+            var documentHeight = document.body.scrollHeight,
+                windowHeight = window.innerHeight,
+                target = component.find('prefooter');
+            if ((documentHeight > windowHeight) && component.get('v.defineLargeContentSize')) {
+                return;
             }
-        } else {
-            if (documentHeight === windowHeight || documentHeight === 0) {
-                if (!component.get('v.defineSmallContentSize')) {
-                    $A.util.addClass(target, 'smallContent');
-                    component.set('v.defineSmallContentSize', true);
+            if (window.location.href.includes('profile')) {
+                if (documentHeight > windowHeight) {
+                    target.getElement().style.display = 'block';
+                } else {
+                    target.getElement().style.display = 'none';
                 }
             } else {
-                $A.util.removeClass(target, 'smallContent');
-                component.set('v.defineLargeContentSize', true);
+                if (documentHeight === windowHeight || documentHeight === 0) {
+                    if (!component.get('v.defineSmallContentSize')) {
+                        $A.util.addClass(target, 'smallContent');
+                        component.set('v.defineSmallContentSize', true);
+                    }
+                } else {
+                    $A.util.removeClass(target, 'smallContent');
+                    component.set('v.defineLargeContentSize', true);
+                }
             }
         }
 
