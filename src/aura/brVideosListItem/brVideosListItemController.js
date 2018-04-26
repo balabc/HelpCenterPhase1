bde801e69fada90daf8f645c3e01e260858283f9
Dmitry Omelchenko <lovandos@gmail.com>
2018-04-26 17:08:33 +0300

    navigate to Guides/Videos article when preview image clicked on list view

--- src/aura/brVideosListItem/brVideosListItemController.js
+++ src/aura/brVideosListItem/brVideosListItemController.js
@@ -1,6 +1,9 @@
 ({
-    doInit : function(cmp) {
-        cmp.set('v.routeInput', {recordId: cmp.get('v.videoArticleForCurrentPage').Id});
+    doInit : function(component) {
+        var objSfdcSite = $A.get('$SfdcSite');
+
+        objSfdcSite = (!!objSfdcSite ? objSfdcSite.pathPrefix : '');
+        component.set('v.routeInput', {recordId: objSfdcSite + '/s/article/' + component.get('v.videoArticleForCurrentPage').UrlName});
     },
 
     onClick : function(cmp) {
